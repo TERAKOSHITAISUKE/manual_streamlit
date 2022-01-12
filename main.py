d28e55fe1c29f4d59fe46eb932a04b5bd0892dcc
TERAKOSHITAISUKE <denpsyadvance@gmail.com>
2022-01-12 21:59:18 +0900

    notion rss

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ from bs4 import BeautifulSoup
 import streamlit as st
 import pandas as pd
 
-# @st.cache
-def get_data(url):
-    res = requests.get(url)
-    soup = BeautifulSoup(res.text, 'html.parser')
 
-    article = soup.find_all(href=re.compile("article/"))
-    # print(article)
+# @st.cache
+# def get_data(url):
+#     res = requests.get(url)
+#     soup = BeautifulSoup(res.text, 'html.parser')
+
+#     article = soup.find_all(href=re.compile("article/"))
+#     # print(article)
     
-    list = []
-    for el in article:
-        try:
-            list.append(el.contents[3].text)
-            # print("-"*10)
-        except:
-            break        
-    return list
+#     list = []
+#     for el in article:
+#         try:
+#             list.append(el.contents[3].text)
+#             # print("-"*10)
+#         except:
+#             break        
+#     return list
 
 url = 'https://www.hokkaido-np.co.jp/news/n_economy'
 
